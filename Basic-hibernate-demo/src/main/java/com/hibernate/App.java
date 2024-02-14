c27083b31a81628e65414defe43127d30cea42a5
Shivam Punia <42895980+workwithshivi@users.noreply.github.com>
2024-02-14 23:44:38 +0530

    initial commit

---
+++ Basic-hibernate-demo/src/main/java/com/hibernate/App.java
@@ -0,0 +1,14 @@
+package com.hibernate;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App 
+{
+    public static void main( String[] args ) {
+
+
+
+    }
+}
